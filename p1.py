cfb0926e627b9aebeb7bc0e621aecfe68bc8d72b
Anjal Thapa <71475450+Aerackkkkk@users.noreply.github.com>
2022-07-16 17:45:48 +0545

    Add files via upload
    Python testing

---
+++ p1.py
@@ -0,0 +1,7 @@
+x,y,z="Anjal","Thapa","Aerackk"
+def myfunc():
+    x="Aerack"
+    print("Hello World, my name is", x +"my surname is", y +"and my nickname is", y)
+myfunc()
+print("Hello World, my name is", x )
+
