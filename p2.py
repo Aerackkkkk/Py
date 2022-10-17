495fca94c639def345148b2a925c8935fe28adea
Anjal Thapa <71475450+Aerackkkkk@users.noreply.github.com>
2022-10-17 16:12:31 +0545

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,13 @@
+#Simple arithmetic
+A=input("Enter value of A:")
+B=input("Enter value of B:")
+A=int(A)
+B=int(B)
+sum=(A+B)
+mul=(A*B)
+sub=(A-B)
+avg=(A+B)/2
+print("The sum of two numbers is:", sum)
+print("The multiplication of two numbers is:", mul)
+print("The subtraction of two numbers is:", sub)
+print("The average of two numbers is:", avg)
